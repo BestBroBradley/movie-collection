065255fe498408c4c8e324b3b5d76eea464db5c0
Allison Bradley <allison.bradley@outlook.com>
2020-05-06 17:29:14 -0600

    Fixed framework

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-menu = "You can add a movie (a), list all movies (l), search for a title (s), or quit (q)."
-user_selection = input(menu)
+menu = "You can add a movie (a), list all movies (l), search for a title (s), or quit (q):"
 
-if user_selection == "a":
-    print("add movie")
-    input(menu)
-elif user_selection == "l":
-    print("see all")
-    input(menu)
-elif user_selection == "s":
-    print("search for a title")
-    input(menu)
-elif user_selection == "q":
-    print("Thank you for using our movie service")
-else:
-    print("You must select a valid option.")
-    print(menu)
+def get_selection():
+    user_selection = input(menu)
+    if user_selection == "a":
+      print("add movie")
+      get_selection()
+    elif user_selection == "l":
+      print("see all")
+      get_selection()
+    elif user_selection == "s":
+      print("search for a title")
+      get_selection()
+    elif user_selection == "q":
+      print("Thank you for using our movie service")
+    else:
+      print("You must select a valid option.")
+      get_selection()
+
+get_selection()
