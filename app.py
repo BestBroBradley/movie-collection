3f7b2390809071b43622616c4f293f415cc620c7
Allison Bradley <allison.bradley@outlook.com>
2020-05-06 18:38:39 -0600

    Added search functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-menu = "You can add a movie (a), list all movies (l), search for a title (s), or quit (q):"
+menu = "You can add a movie (a), list all movies (l), search for a title (s), or quit (q): "
 
 user_library = []
 
@@ -11,18 +11,20 @@ def query_user():
       view_all()
       query_user()
     elif user_selection == "s":
-      print("search for a title")
+      search()
       query_user()
     elif user_selection == "q":
-      print("Thank you for using our movie service")
+      print("Thank you for using our movie service.")
     else:
       print("You must select a valid option.")
       query_user()
+
 
 def get_gen_rating(new_title):
     genre = input(f"What genre is {new_title}? ")
     rating = input(f"What rating would you like to give {new_title}? (?/10) ")
     verify_gen_rating(new_title, genre, rating)
+
 
 def verify_title(new_title):
     verify_query = input(f"Would you like to add {new_title} to your library? (y/n) ")
@@ -34,21 +36,25 @@ def verify_title(new_title):
         print("You must type either 'y' or 'n'.")
         verify_title(new_title)
 
+def add_to_library(new_title, genre, rating):
+    user_library.append({"title": new_title, "genre": genre, "rating": rating + "/10"})
+    query_user()
+
 def verify_gen_rating(new_title, genre, rating):
     verify_final = input(f"Would you like to add {new_title} with a genre of {genre} and a rating of {rating}/10 to your library? (y/n) ")
     if verify_final == "y":
-        user_library.append({"title": new_title, "genre": genre, "rating": rating + "/10"})
-        print(user_library)
-        query_user()
+        add_to_library(new_title, genre, rating)
     elif verify_final == "n":
         get_gen_rating(new_title)
     else:
         print("You must type either 'y' or 'n'.")
         verify_gen_rating(new_title, genre, rating)
 
+
 def add_movie():
     new_title = input("What is the title of the movie you would like to add? ")
     verify_title(new_title)
+
 
 def view_all():
     if len(user_library) > 0:
@@ -63,5 +69,17 @@ def view_all():
         print("There are no titles in your library.")
         query_user()
 
+def search():
+    search_term = input("What title would you like to search for? ")
+    for movie in user_library:
+        if movie["title"].lower() == search_term.lower():
+            title = movie["title"]
+            genre = movie["genre"]
+            rating = movie["rating"]
+            print("Movie Details:")
+            print(f"{title} | {genre} | {rating}")
+            break
+    else:
+        print("Looks like that title isn't in your library.")
+    query_user()
 query_user()
-
